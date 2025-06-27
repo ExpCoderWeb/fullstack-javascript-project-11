6b1b0df4b77f39fa2c3687c2d6200435ab7d38de
ExpCoderWeb <shagaev_2011@mail.ru>
2025-06-27 21:17:03 +0400

    set up locales

---
+++ src/js/locales/ru.js
@@ -0,0 +1,33 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    pageTitle: 'RSS Feeds Agreggator',
+    modalReadMoreButton: 'Читать полностью',
+    modalCloseButton: 'Закрыть',
+    heading: 'RSS Агрегатор',
+    slogan: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    urlInput: 'ссылка RSS',
+    urlLabel: 'Ссылка RSS',
+    addUrlButton: 'Добавить',
+    urlExample: 'Пример: https://lorem-rss.hexlet.app/feed',
+    createdBy: 'created by',
+    authorInfo: 'Renat Shagaev',
+    feedsHeading: 'Фиды',
+    postsHeading: 'Посты',
+    postPreviewButton: 'Просмотр',
+    formFeedback: {
+      errors: {
+        network: 'Ошибка сети',
+        validation: {
+          required: 'Не должно быть пустым',
+          invalidUrl: 'Ссылка должна быть валидным URL',
+          alreadyExists: 'RSS уже существует',
+          noRss: 'Ресурс не содержит валидный RSS',
+        },
+      },
+      success: 'RSS успешно загружен',
+    },
+  },
+}
